198af9940bc8bcd4211b25ac358a0c34ac01b3d9
Oumar <osm.cre.dev@gmail.com>
2023-11-01 17:12:12 -0700

    reset password

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -33,4 +33,60 @@
             </div>
         </form>
     </x-jet-authentication-card>
+</x-guest-layout>
+--}}
+<x-guest-layout>
+    <!--main area-->
+    <main id="main" class="main-site left-sidebar">
+
+        <div class="container">
+
+            <div class="wrap-breadcrumb">
+                <ul>
+                    <li class="item-link"><a href="/" class="link">Accueil</a></li>
+                    <li class="item-link"><span>Rénitialiser mot de passe</span></li>
+                </ul>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12 col-md-offset-3">
+                    <div class=" main-content-area">
+                        <div class="wrap-login-item ">                      
+                            <div class="login-form form-item form-stl">
+                                 @if (session('status'))
+                                    <div class="mb-4 font-medium text-sm text-green-600">
+                                        {{ session('status') }}
+                                    </div>
+                                @endif
+                                <x-jet-validation-errors class="mb-4" />
+                                <form name="frm-login" method="POST" action="{{ route('password.update') }}">
+                                    @csrf
+                                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                                    <fieldset class="wrap-title">
+                                        <h3 class="form-title">Rénitialiser mot de passe</h3>                                      
+                                    </fieldset>
+                                    <fieldset class="wrap-input">
+                                        <label for="frm-login-uname">Email:</label>
+                                        <input type="email" id="frm-login-uname" name="email" placeholder="Type your email address" :value="{{ $request->email }}" required autocomplete="">
+                                    </fieldset>
+                                    <fieldset class="wrap-input item-width-in-half left-item ">
+                                        <label for="password">Mot de passe *</label>
+                                        <input type="password" id="password" name="password" required autocomplete="new-password" placeholder="Password">
+                                    </fieldset>
+                                    <fieldset class="wrap-input item-width-in-half ">
+                                        <label for="password_confirmation">Confirmer Mot de passe *</label>
+                                        <input type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirm Password" required autocomplete="new-password">
+                                    </fieldset>
+                                
+                                    <input type="submit" class="btn btn-submit" value="Rénitialiser mot de passe" name="submit">
+                                </form>
+                            </div>                                              
+                        </div>
+                    </div><!--end main products area-->     
+                </div>
+            </div><!--end row-->
+
+        </div><!--end container-->
+
+    </main>
+    <!--main area-->
 </x-guest-layout>
